3e0c559e3fe79af759ce9b17567c26f4fd86de20
karolinneczka <okaa93@gmail.com>
2018-04-16 22:10:36 +0200

    Add basic files and add a simple program in js file to check is the value positive, negative or equal to zero

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+var a = 2;
+var b = 4;
+var value = (a * a) - (2 * a * b) - (b * b);
+
+if (value>=0) {
+
+    if (value>0) {
+        console.log('Wynik dodatni');
+    }
+    else {
+     console.log('Wynik rowny zero');
+    } 
+}
+    
+else {
+    console.log('Wynik ujemny');
+  }
